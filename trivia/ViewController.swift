7fd1ae11341de61c998c88f1ede65f73377f7924
daniel gougler <dannyg10503@icloud.com>
2020-10-08 09:53:30 -0400

    Initial Commit

---
+++ trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trivia
+//
+//  Created by Daniel Gougler on 10/8/20.
+//  Copyright © 2020 goog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
